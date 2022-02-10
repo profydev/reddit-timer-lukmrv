bc7042a3388a2cf19fa3e9c0d9c60ddc00ad09a2
Łukasz Sawoń <lukasz.sawon@eurocert.pl>
2022-02-10 16:16:08 +0100

    Fix eslind errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {
   BrowserRouter as Router, Routes, Route, Link,
 } from 'react-router-dom';
-
 import Home from './Pages/Home';
 import Search from './Pages/Search';
 
@@ -15,20 +14,27 @@ function App() {
         <Link to="/search">Search</Link>
       </header>
 
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
       </Routes>
 
-      <div className="app">
-        App Placeholder
-      </div>
+      <div className="app">App Placeholder</div>
 
-      <footer>
-        This is footer
-      </footer>
+      <Footer />
     </Router>
   );
 }
+
+const Header = () => (
+  <header>
+    <Link to="/">Home</Link>
+    <Link to="/search">Search</Link>
+  </header>
+);
+
+const Footer = () => <footer>This is footer</footer>;
 
 export default App;
