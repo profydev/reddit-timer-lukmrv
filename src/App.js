cd1713dd5843d61735c6b50d8f43a9308ff008b7
Łukasz Sawoń <lukasz.sawon@eurocert.pl>
2022-02-10 14:51:16 +0100

    Added create-app-skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Search from './Pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+
+      <div className="app">
+        App Placeholder
+      </div>
+
+      <footer>
+        This is footer
+      </footer>
+    </Router>
   );
 }
 
